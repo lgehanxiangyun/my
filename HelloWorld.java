978f23983292565a9b1825ed039ad06f90f4372e
xiangyun.han <xiangyun.han@lge.com>
2014-01-05 21:18:46 -0500

    review HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,10 @@
+import android.app.Activity;
+import android.os.Bundle;
+
 class HelloWorld extends Activity{
-  
+ public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+ }  
 
 }
